5d02c517d212f75ae636ca5ee89421550cf31f92
GoldenElk94 <michaelpricesr4@gmail.com>
2018-05-31 14:59:48 -0400

    Nested Loops
    Simple examples of Nested Loops

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Nested loop Example
+// for(outercounter = 0; outercounter < 3; outercounter ++) {
+//     for(innercounter = 0; innercounter <2; innercounter ++) {
+//         alert(outercounter + " " + innercounter);
+//     }
+// }
+// output = 00, 01, 10, 11, 20, 21
+
+// Practice Javascript Nested loop slide # 17
+
+// var innerLoopCounter;
+// var outerLoopCounter;
+// var sum = 0;
+
+// for(outerLoopCounter = 0; outerLoopCounter < 3; outerLoopCounter ++ ) {
+//     for(innerLoopCounter = 0; innerLoopCounter < 4; innerLoopCounter ++) {
+//         sum += innerLoopCounter;
+//         alert(sum);
+//     }
+// }
+// output = 0, 1, 3, 6, 6, 7, 9, 12, 12,  13, 15, 18
+
+// example 2
+
+// var innerLoopCounter;
+// var outerLoopCounter;
+// var sum = 0;
+
+// for(outerLoopCounter = 0; outerLoopCounter < 5; outerLoopCounter ++ ) {
+//     for(innerLoopCounter = 0; innerLoopCounter < 2; innerLoopCounter ++) {
+//         sum += innerLoopCounter;
+//         alert(sum);
+//     }
+// }
+//  output = 0, 1, 1, 2, 2, 3, 3, 4, 5s
